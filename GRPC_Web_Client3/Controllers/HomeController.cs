dea4264e315aa26b1057a480ac065d1d077bcb1d
Kiranmali0505 <69839113+Kiranmali0505@users.noreply.github.com>
2020-08-19 11:58:15 +0530

    Add files via upload
    grpc client

---
+++ GRPC_Web_Client3/Controllers/HomeController.cs
@@ -0,0 +1,121 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using GRPC_Web_Client3.Models;
+using Grpc.Net.Client;
+using GrpcServer.Protos;
+using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+using Grpc.Core;
+
+using System.Web;
+
+namespace GRPC_Web_Client3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public IConfiguration Configuration { get; set; }
+     
+        private static string _token;
+        private string _server;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            Configuration = configuration;
+        }
+
+        public async Task<IActionResult>  Index()
+            {
+     
+            string channelTarget = Configuration["ServerURL"];
+            _token = await Authenticate();
+            var channel = CreateAuthenticatedChannel(channelTarget);
+           
+           
+            var userClient = new User.UserClient(channel);
+
+            var clientreq = new UserInput { UserId=4 };
+            var user = await userClient.GetUserInfoAsync(clientreq);
+            var jsonResult = Json(new { getAllResult = user });
+            return jsonResult;
+        }
+        private static GrpcChannel CreateAuthenticatedChannel(string address)
+        {
+           
+            var credentials = CallCredentials.FromInterceptor((context, metadata) =>
+            {
+                if (!string.IsNullOrEmpty(_token))
+                {
+                    metadata.Add("Authorization", $"Bearer {_token}");
+                }
+                return Task.CompletedTask;
+
+            });
+
+            // SslCredentials is used here because this channel is using TLS.
+
+            // Channels that aren't using TLS should use ChannelCredentials.Insecure instead.
+            var httpHandler = new HttpClientHandler();
+
+            httpHandler.ServerCertificateCustomValidationCallback =
+                HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+           
+            var channel = GrpcChannel.ForAddress(address, new GrpcChannelOptions
+            {
+                Credentials = ChannelCredentials.Create(new SslCredentials(), credentials),
+                HttpHandler = httpHandler
+            });
+
+            return channel;
+
+        }
+        private static async Task<string> Authenticate()
+
+        {
+            try {
+                Console.WriteLine($"Authenticating as {Environment.UserName}...");
+
+                var httpClient = new HttpClient();
+
+                var request = new HttpRequestMessage
+
+                {
+
+                    RequestUri = new Uri($" https://localhost:5001/generateJwtToken?name={HttpUtility.UrlEncode(Environment.UserName)}"),
+
+                    Method = HttpMethod.Get,
+
+                    Version = new Version(2, 0)
+
+                };
+            
+                var tokenResponse = await httpClient.SendAsync(request);
+
+                tokenResponse.EnsureSuccessStatusCode();
+                var token = await tokenResponse.Content.ReadAsStringAsync();
+                Console.WriteLine("Successfully authenticated.");
+                return token;
+            } 
+            catch (Exception ex) {
+                throw ex;
+                
+            }
+
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ GRPC_Web_Client3/Startup.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace GRPC_Web_Client3
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(),"Properties","launchSetting.json"));
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddSingleton<IConfiguration>(Configuration);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
